cba8b6e084b1b3ecf8b112c02b9d2570efe44377
IlyaKachnov <iliya.kachnov@gmail.com>
2018-02-08 18:25:41 +0300

    Tables added

---
+++ src/main/resources/public/js/forms/leagues-form.js
@@ -0,0 +1,39 @@
+//== Class definition
+
+var FormControls = function () {
+    //== Private functions
+
+    var leaguesForm = function () {
+        $( "#m_form_1" ).validate({
+            // define validation rules
+            rules: {
+                name: {
+                    required: true,
+                    // minlength: 10
+                },
+            },
+
+            //display error alert on form submit
+            invalidHandler: function(event, validator) {
+                var alert = $('#m_form_1_msg');
+                alert.removeClass('m--hide').show();
+                mApp.scrollTo(alert, -200);
+            },
+
+            submitHandler: function (form) {
+                //form[0].submit(); // submit the form
+            }
+        });
+    }
+
+    return {
+        // public functions
+        init: function() {
+            leaguesForm();
+        }
+    };
+}();
+
+jQuery(document).ready(function() {
+    FormControls.init();
+});
