f98e6cf67491f5d868841f34e0228dc10fecbce7
Ilya <iliya.kachnov@gmail.com>
2018-03-23 01:20:00 +0300

    Adaptive for mobiles & tabs now

--- src/main/resources/public/js/custom/send-email.js
+++ src/main/resources/public/js/custom/send-email.js
@@ -2,6 +2,7 @@ $(document).ready(function () {
     $("#send-email").on('click', function (e) {
         e.preventDefault();
         var sendUrl = $(this).attr('href');
+        swal({text:"Успешно отправлено!"});
         $.ajax({
             type:"GET",
             url: sendUrl,
